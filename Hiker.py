ff3bb383cb94baf58d83d197181510558be835e2
Sydney Hildreth <srhildre@uvm.edu>
2020-10-24 23:05:14 -0400

    Started Hiker Class

---
+++ Hiker.py
@@ -0,0 +1,16 @@
+
+class Hiker:
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+
+    def get_id(self):
+        return self._id
+
+    def get_name(self):
+        return self._name
+
+
+
+
+    
