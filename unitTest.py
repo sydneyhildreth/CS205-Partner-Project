d81ab97e7cd069b4f5480863c6e4c6d5e2e31828
Sidhanth Kafley <skafley@uvm.edu>
2020-11-07 14:03:24 -0500

    Create unit tests and fix bug in trail.py

--- unitTest.py
+++ unitTest.py
@@ -5,7 +5,7 @@ import unittest
 
 class testStatus(unittest.TestCase):
 
-
+    @classmethod
     def setUpClass(cls):
         print('setUpClass()')
         cls.HikingLog = HikingLog.HikingLog().get()
@@ -26,17 +26,17 @@ class testStatus(unittest.TestCase):
         cls.trail2 = Trail.Trail(trail2Name, trail2Location, trail2Elevation, trail2Rank)
         cls.trail3 = Trail.Trail(trail3Name, trail3Location, trail3Elevation, trail3Rank)
 
-        cls.sid = Hiker.Hiker(1, 'Sid')
-        cls.syd = Hiker.Hiker(2, 'Syd')
+        cls.sid = Hiker.Hiker('Sid')
+        cls.syd = Hiker.Hiker('Syd')
 
-        cls.HikingLog.addTrail(cls.Trail1)
-        cls.HikingLog.add_book(cls.Trail2)
-        cls.HikingLog.add_book(cls.Trail3)
+        cls.HikingLog.addTrail(cls.trail1)
+        cls.HikingLog.addTrail(cls.trail2)
+        cls.HikingLog.addTrail(cls.trail3)
 
         cls.HikingLog.addHiker(cls.sid)
         cls.HikingLog.addHiker(cls.syd)
 
-
+    @classmethod
     def tearDownClass(cls):
         # called one time, at end
         print('tearDownClass()')
@@ -55,7 +55,7 @@ class testStatus(unittest.TestCase):
     def testStatusOne(self):
         print('testStatusone()')
         # check that the Hiking Log shows that no trails are hiked by Sid
-        trails = self.HikingLog.getStatus(self, self.sid)
+        trails = self.HikingLog.getStatus(self.sid)
         self.assertEqual(len(trails), 0)
 
         # Update Status for Sid
@@ -67,7 +67,7 @@ class testStatus(unittest.TestCase):
 
         # check that the book checked out to john is book1
         if len(trails) == 1:
-            self.assertEqual(trails[0], self.trail1)
+            self.assertEqual(trails[0], self.trail1.toString())
 
         # check that john shows one book checked out
         trails = self.sid.getStatus()
@@ -76,3 +76,31 @@ class testStatus(unittest.TestCase):
         # check that the book checked out to john is book1
         if len(trails) == 1:
             self.assertEqual(trails[0], self.trail1)
+
+
+    def testStatusTwo(self):
+        print('testStatusTwo()')
+        # Let Syd hike
+        s = self.HikingLog.updateStatus(self.syd, self.trail2)
+        self.assertIsNotNone(s)
+
+        # check that the library shows one book checked out to mary
+        trails = self.HikingLog.getStatus(self.syd)
+        self.assertEqual(len(trails), 1)
+
+        # check that the book checked out to mary is book2
+        if len(trails) == 1:
+            self.assertEqual(trails[0], self.trail2.toString())
+
+        # check that mary shows one book checked out
+        trails = self.syd.getStatus()
+        self.assertEqual(len(trails), 1)
+
+        # check that the book checked out to mary is book2
+        if len(trails) == 1:
+            self.assertEqual(trails[0], self.trail2)
+
+
+if __name__ == "__main__":
+  unittest.main()
+
